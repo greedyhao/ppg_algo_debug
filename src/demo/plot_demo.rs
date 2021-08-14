623452a0f262606942676ac8fdc8e42ca5ac0c7c
greedyhao <hao_kr@163.com>
2021-08-14 16:50:38 +0800

    清楚一些不要的图形

--- src/demo/plot_demo.rs
+++ src/demo/plot_demo.rs
@@ -4,14 +4,12 @@ use plot::{
     Arrows, Corner, HLine, Legend, Line, LineStyle, MarkerShape, Plot, PlotImage, Points, Polygon,
     Text, VLine, Value, Values,
 };
-use std::f64::consts::TAU;
+// use std::f64::consts::TAU;
 
 #[derive(PartialEq)]
-pub struct LineDemo {
+struct LineDemo {
     animate: bool,
     time: f64,
-    circle_radius: f64,
-    circle_center: Pos2,
     square: bool,
     proportional: bool,
     line_style: LineStyle,
@@ -23,12 +21,10 @@ impl Default for LineDemo {
         Self {
             animate: !cfg!(debug_assertions),
             time: 0.0,
-            circle_radius: 1.5,
-            circle_center: Pos2::new(0.0, 0.0),
             square: false,
             proportional: true,
             line_style: LineStyle::Solid,
-            open: false,
+            open: true,
         }
     }
 }
@@ -38,8 +34,6 @@ impl LineDemo {
         let Self {
             animate,
             time: _,
-            circle_radius,
-            circle_center,
             square,
             proportional,
             line_style,
@@ -47,30 +41,6 @@ impl LineDemo {
         } = self;
 
         ui.horizontal(|ui| {
-            ui.group(|ui| {
-                ui.vertical(|ui| {
-                    ui.label("Circle:");
-                    ui.add(
-                        egui::DragValue::new(circle_radius)
-                            .speed(0.1)
-                            .clamp_range(0.0..=f64::INFINITY)
-                            .prefix("r: "),
-                    );
-                    ui.horizontal(|ui| {
-                        ui.add(
-                            egui::DragValue::new(&mut circle_center.x)
-                                .speed(0.1)
-                                .prefix("x: "),
-                        );
-                        ui.add(
-                            egui::DragValue::new(&mut circle_center.y)
-                                .speed(1.0)
-                                .prefix("y: "),
-                        );
-                    });
-                });
-            });
-
             ui.vertical(|ui| {
                 ui.style_mut().wrap = Some(false);
                 ui.checkbox(animate, "animate");
@@ -79,6 +49,7 @@ impl LineDemo {
                 ui.checkbox(proportional, "Proportional data axes")
                     .on_hover_text("Tick are the same size on both axes.");
             });
+
             ui.vertical(|ui| {
                 ComboBox::from_label("Line style")
                     .selected_text(line_style.to_string())
@@ -99,34 +70,6 @@ impl LineDemo {
         });
     }
 
-    fn circle(&self) -> Line {
-        let n = 512;
-        let circle = (0..=n).map(|i| {
-            let t = remap(i as f64, 0.0..=(n as f64), 0.0..=TAU);
-            let r = self.circle_radius;
-            Value::new(
-                r * t.cos() + self.circle_center.x as f64,
-                r * t.sin() + self.circle_center.y as f64,
-            )
-        });
-        Line::new(Values::from_values_iter(circle))
-            .color(Color32::from_rgb(100, 200, 100))
-            .style(self.line_style)
-            .name("circle")
-    }
-
-    fn sin(&self) -> Line {
-        let time = self.time;
-        Line::new(Values::from_explicit_callback(
-            move |x| 0.5 * (2.0 * x).sin() * time.sin(),
-            ..,
-            512,
-        ))
-        .color(Color32::from_rgb(200, 100, 100))
-        .style(self.line_style)
-        .name("wave")
-    }
-
     fn sample(&self) -> Line {
         Line::new(Values::from_parametric_callback(
             move |t| (t, 1.0),
@@ -136,18 +79,6 @@ impl LineDemo {
         .color(Color32::from_rgb(200, 100, 100))
         .style(self.line_style)
         .name("sample")
-    }
-
-    fn thingy(&self) -> Line {
-        let time = self.time;
-        Line::new(Values::from_parametric_callback(
-            move |t| ((2.0 * t + time).sin(), (3.0 * t).sin()),
-            0.0..=TAU,
-            256,
-        ))
-        .color(Color32::from_rgb(100, 150, 250))
-        .style(self.line_style)
-        .name("x = sin(2t), y = sin(3t)")
     }
 }
 
@@ -159,9 +90,6 @@ impl Widget for &mut LineDemo {
             self.time += ui.input().unstable_dt.at_most(1.0 / 30.0) as f64;
         };
         let mut plot = Plot::new("lines_demo")
-            .line(self.circle())
-            .line(self.sin())
-            .line(self.thingy())
             .line(self.sample())
             .legend(Legend::default());
         if self.square {
@@ -197,7 +125,7 @@ impl super::Demo for PlotDemo {
     }
 }
 
-use rfd::*;
+use rfd::FileDialog;
 use crate::demo::View;
 impl super::View for PlotDemo {
     fn ui(&mut self, ui: &mut Ui) {
